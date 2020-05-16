0640b311090be0a905a7c6289856033c47a079f2
VivekSharma811 <vs811861@gmail.com>
2020-05-16 17:37:26 +0530

    API added

--- app/src/main/java/com/devtides/androidcoroutinesflow/model/NewsArticle.kt
+++ app/src/main/java/com/devtides/androidcoroutinesflow/model/NewsArticle.kt
@@ -1,10 +1,13 @@
 package com.devtides.androidcoroutinesretrofit.model
 
+import com.google.gson.annotations.SerializedName
+
 
 data class NewsArticle(
     val author: String? = null,
     val title: String? = null,
     val description: String? = null,
+    @SerializedName("imageUrl")
     val url: String? = null,
     val urlToImage: String? = null,
     val publishedAt: String? = null
